def40aa55d59a8d6dc763b3696aa5221e15896d3
Charlotte <charlotte.jondet@iut-valence.fr>
2015-10-06 14:50:17 +0200

    Extracted constants

--- src/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRose.java
+++ src/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRose.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class GildedRose
 {
 
+	private static final int SELLIN_THRESHOLD_FOR_BACKSTAGE_QUALITY_INCREASING_THREE_TIMES_AS_FAST = 5;
+	private static final int SELLIN_THRESHOLD_FOR_BACKSTAGE_QUALITY_INCREASING_TWICE_AS_FAST = 10;
+	private static final int MAX_QUALITY = 50;
 	private static List<Item> items = null;
 
 	/**
@@ -17,9 +20,9 @@ public class GildedRose
 		System.out.println("OMGHAI!");
 
 		items = new ArrayList<Item>();
-		items.add(new Item("+5 Dexterity Vest", 10, 20));
+		items.add(new Item("+5 Dexterity Vest", SELLIN_THRESHOLD_FOR_BACKSTAGE_QUALITY_INCREASING_TWICE_AS_FAST, 20));
 		items.add(new Item("Aged Brie", 2, 0));
-		items.add(new Item("Elixir of the Mongoose", 5, 7));
+		items.add(new Item("Elixir of the Mongoose", SELLIN_THRESHOLD_FOR_BACKSTAGE_QUALITY_INCREASING_THREE_TIMES_AS_FAST, 7));
 		items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
 		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
 		items.add(new Item("Conjured Mana Cake", 3, 6));
@@ -58,23 +61,23 @@ public class GildedRose
 		}
 		else
 		{
-			if (item.getQuality() < 50)
+			if (item.getQuality() < MAX_QUALITY)
 			{
 				item.setQuality(item.getQuality() + 1);
 
 				if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
 				{
-					if (item.getSellIn() < 10)
+					if (item.getSellIn() < SELLIN_THRESHOLD_FOR_BACKSTAGE_QUALITY_INCREASING_TWICE_AS_FAST)
 					{
-						if (item.getQuality() < 50)
+						if (item.getQuality() < MAX_QUALITY)
 						{
 							item.setQuality(item.getQuality() + 1);
 						}
 					}
 
-					if (item.getSellIn() < 5)
+					if (item.getSellIn() < SELLIN_THRESHOLD_FOR_BACKSTAGE_QUALITY_INCREASING_THREE_TIMES_AS_FAST)
 					{
-						if (item.getQuality() < 50)
+						if (item.getQuality() < MAX_QUALITY)
 						{
 							item.setQuality(item.getQuality() + 1);
 						}
@@ -104,7 +107,7 @@ public class GildedRose
 			}
 			else
 			{
-				if (item.getQuality() < 50)
+				if (item.getQuality() < MAX_QUALITY)
 				{
 					item.setQuality(item.getQuality() + 1);
 				}
